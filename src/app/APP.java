69138c23153ee2558c3b50842f97185e6af05552
wengshengyuan <wengshengyuan@gmail.com>
2016-02-28 14:46:27 +0800

    #

--- src/app/APP.java
+++ src/app/APP.java
@@ -11,23 +11,26 @@ import tasks.TaskOne;
 public class APP {
 	
 	public static void main(String[] args) {
-//		new TaskOne(1).enableExeLimit().enableTimeLimit().setTimeLimit(1000L).setExeLimit(3).setInterval(30L).start();
-//		new TaskOne().enableTimeLimit().setTimeLimit(5500L).setInterval(30L).start();
-		BlockingQueue queue = new LinkedBlockingQueue<Runnable>(3);
+		BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>(3);
 		ThreadPoolExecutor pool = new ThreadPoolExecutor(3, 5, 2L,TimeUnit.SECONDS, queue, new ThreadPoolExecutor.CallerRunsPolicy());
 		SysoutCallable callable = new SysoutCallable();
 		
 		for(int i = 0; i < 10; i  ++){
 			try {
-//				pool.execute(new TaskOne(i).enableExeLimit().setExeLimit(3).setInterval(1000L));
-				pool.execute(new TaskOne(i, callable).enableTimeLimit().setTimeLimit(2000L).setInterval(1000L));
+				pool.execute(new TaskOne(i, callable)
+						.enableTimeLimit()
+						.setTimeLimit(2000L)
+						.setInterval(1000L));
 				Thread.sleep(10L);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		
-		pool.execute(new TaskOne(10, callable).enableExeLimit().setExeLimit(3).setInterval(500L));
+		pool.execute(new TaskOne(10, callable)
+				.enableExeLimit()
+				.setExeLimit(3)
+				.setInterval(500L));
 		
 		while(true){
 			try {
