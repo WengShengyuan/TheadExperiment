bf60681745fbf7a698da0746da68ae5c984b40b1
wengshengyuan <wengshengyuan@gmail.com>
2016-02-28 11:11:57 +0800

    #

--- src/app/APP.java
+++ src/app/APP.java
@@ -15,19 +15,19 @@ public class APP {
 //		new TaskOne().enableTimeLimit().setTimeLimit(5500L).setInterval(30L).start();
 		BlockingQueue queue = new LinkedBlockingQueue<Runnable>(3);
 		ThreadPoolExecutor pool = new ThreadPoolExecutor(3, 5, 2L,TimeUnit.SECONDS, queue, new ThreadPoolExecutor.CallerRunsPolicy());
-		
+		SysoutCallable callable = new SysoutCallable();
 		
 		for(int i = 0; i < 10; i  ++){
 			try {
 //				pool.execute(new TaskOne(i).enableExeLimit().setExeLimit(3).setInterval(1000L));
-				pool.execute(new TaskOne(i, new SysoutCallable()).enableTimeLimit().setTimeLimit(2000L).setInterval(1000L));
+				pool.execute(new TaskOne(i, callable).enableTimeLimit().setTimeLimit(2000L).setInterval(1000L));
 				Thread.sleep(10L);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		
-		pool.execute(new TaskOne(10, new SysoutCallable()).enableExeLimit().setExeLimit(3).setInterval(500L));
+		pool.execute(new TaskOne(10, callable).enableExeLimit().setExeLimit(3).setInterval(500L));
 		
 		while(true){
 			try {
